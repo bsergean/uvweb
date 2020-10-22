ca4b56aa851dfcfa3efa0866b6a2654d3b2464c5
Benjamin Sergeant <bsergeant@mz.com>
2020-10-21 20:35:14 -0700

    capture http request content into a request object

--- main.cpp
+++ main.cpp
@@ -4,45 +4,66 @@
 #include <cstring>
 #include <uvw.hpp>
 
+#include <map>
+#include <memory>
+#include <functional>
+
+using OnMessageCompleteCallback = std::function<void()>;
+
+struct Request
+{
+    std::map<std::string, std::string> headers;
+    std::string currentHeaderName;
+    std::string currentHeaderValue;
+    std::string url;
+    std::string body;
+    bool messageComplete = false;
+};
+
 int on_message_begin(http_parser*)
 {
-    std::cout << "Start parsing message" << std::endl;
     return 0;
 }
 
-int on_url(http_parser*, const char* at, const size_t length)
+int on_url(http_parser* parser, const char* at, const size_t length)
 {
-    std::cout << "Get url " << std::string(at, length) << std::endl;
+    Request* request = reinterpret_cast<Request*>(parser->data);
+    request->url = std::string(at, length);
     return 0;
 }
 
-int on_headers_complete(http_parser*)
+int on_headers_complete(http_parser* parser)
 {
-    std::cout << "Header complete" << std::endl;
+    Request* request = reinterpret_cast<Request*>(parser->data);
+    request->headers[request->currentHeaderName] = request->currentHeaderValue;
     return 0;
 }
 
-int on_message_complete(http_parser*)
+int on_message_complete(http_parser* parser)
 {
-    std::cout << "Message complete" << std::endl;
+    Request* request = reinterpret_cast<Request*>(parser->data);
+    request->messageComplete = true;
     return 0;
 }
 
-int on_header_field(http_parser*, const char* at, const size_t length)
+int on_header_field(http_parser* parser, const char* at, const size_t length)
 {
-    std::cout << "Get header field " << std::string(at, length) << std::endl;
+    Request* request = reinterpret_cast<Request*>(parser->data);
+    request->currentHeaderName = std::string(at, length);
     return 0;
 }
 
-int on_header_value(http_parser*, const char* at, const size_t length)
+int on_header_value(http_parser* parser, const char* at, const size_t length)
 {
-    std::cout << "Get header value " << std::string(at, length) << std::endl;
+    Request* request = reinterpret_cast<Request*>(parser->data);
+    request->currentHeaderValue = std::string(at, length);
     return 0;
 }
 
-int on_body(http_parser*, const char* at, const size_t length)
+int on_body(http_parser* parser, const char* at, const size_t length)
 {
-    std::cout << "Get body " << std::string(at, length) << std::endl;
+    Request* request = reinterpret_cast<Request*>(parser->data);
+    request->body = std::string(at, length);
     return 0;
 }
 
@@ -73,29 +94,35 @@ int main()
         http_parser* parser = (http_parser*) malloc(sizeof(http_parser));
         http_parser_init(parser, HTTP_REQUEST);
 
-        parser->data = client.get();
+        auto request = std::make_shared<Request>();
+        client->data(request);
+
+        parser->data = client->data().get();
 
         client->on<uvw::DataEvent>(
-            [parser, &settings](const uvw::DataEvent& event, uvw::TCPHandle& client) {
-                /* data received */
-                std::cout.write(event.data.get(), event.length) << std::endl;
-                std::cout << "data length: " << event.length << std::endl;
-
-                size_t len = 80 * 1024, nparsed;
-                char buf[len];
-                ssize_t recved;
-
-                // nparsed = http_parser_execute(parser, &settings, buf, recved);
-                nparsed = http_parser_execute(parser, &settings, event.data.get(), event.length);
-
+            [request, parser, &settings](const uvw::DataEvent& event, uvw::TCPHandle& client) {
+                int nparsed = http_parser_execute(parser,
+                                                  &settings,
+                                                  event.data.get(),
+                                                  event.length);
                 if (nparsed != event.length)
                 {
+                    // FIXME: return 400 here
                     std::cerr << "PARSING ERROR" << std::endl;
+                    return;
                 }
 
-                // auto dataWrite = std::unique_ptr<char[]>(new char[]{ 'h', 'e', 'l', 'l', 'o' });
-                // client.write(std::move(dataWrite), 5);
-                // client.close();
+                // Write response
+                if (request->messageComplete)
+                {
+                    std::cout << "Message complete" << std::endl;
+                    std::cout << "Message length: " << request->body.size()
+                              << std::endl;
+
+                    auto dataWrite = std::unique_ptr<char[]>(new char[]{ 'h', 'e', 'l', 'l', 'o' });
+                    client.write(std::move(dataWrite), 5);
+                    client.close();
+                }
             });
 
         srv.accept(*client);
