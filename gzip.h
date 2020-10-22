7dc142e1e5889f3fa065d3d7a25e8c521b6b305b
Benjamin Sergeant <bsergeant@mz.com>
2020-10-21 23:23:07 -0700

    move gzip code into its own file

---
+++ gzip.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+bool gzipDecompress(const std::string& in, std::string& out);
--- main.cpp
+++ main.cpp
@@ -1,61 +1,16 @@
 
 #include "http_parser.h"
 #include "options.h"
+#include "gzip.h"
+
 #include <cstring>
-#include <functional>
 #include <iostream>
 #include <map>
 #include <memory>
 #include <spdlog/spdlog.h>
 #include <sstream>
 #include <uvw.hpp>
-#include <zlib.h>
 
-
-bool gzipDecompress(const std::string& in, std::string& out)
-{
-    z_stream inflateState;
-    memset(&inflateState, 0, sizeof(inflateState));
-
-    inflateState.zalloc = Z_NULL;
-    inflateState.zfree = Z_NULL;
-    inflateState.opaque = Z_NULL;
-    inflateState.avail_in = 0;
-    inflateState.next_in = Z_NULL;
-
-    if (inflateInit2(&inflateState, 16 + MAX_WBITS) != Z_OK)
-    {
-        return false;
-    }
-
-    inflateState.avail_in = (uInt) in.size();
-    inflateState.next_in = (unsigned char*) (const_cast<char*>(in.data()));
-
-    const int kBufferSize = 1 << 14;
-    std::array<unsigned char, kBufferSize> compressBuffer;
-
-    do
-    {
-        inflateState.avail_out = (uInt) kBufferSize;
-        inflateState.next_out = &compressBuffer.front();
-
-        int ret = inflate(&inflateState, Z_SYNC_FLUSH);
-
-        if (ret == Z_NEED_DICT || ret == Z_DATA_ERROR || ret == Z_MEM_ERROR)
-        {
-            inflateEnd(&inflateState);
-            return false;
-        }
-
-        out.append(reinterpret_cast<char*>(&compressBuffer.front()),
-                   kBufferSize - inflateState.avail_out);
-    } while (inflateState.avail_out == 0);
-
-    inflateEnd(&inflateState);
-    return true;
-}
-
-using OnMessageCompleteCallback = std::function<void()>;
 
 struct Request
 {
